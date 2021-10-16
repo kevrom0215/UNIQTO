01b92cbc6c5b6181fdb08a6d462cd62a59ff857d
kevrom0215 <kennetharaga@gmail.com>
2021-10-16 18:19:33 +0800

    updated product routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
       },
       {path: 'cart', component: CartComponent},
       // Please delete after use
-      {path: 'product-details', component: ProductDetailsComponent},
+      {path: 'product-details/{{product.productName}}', component: ProductDetailsComponent},
       {path: 'welcome', component: ProductListComponent},
       {path: 'about-us', component: AboutUsComponent},
       {path: '', redirectTo:'welcome', pathMatch:'full'},
